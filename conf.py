95d0231193270bb08451fed6be5563205cc1bd01
carlacastilhos <53014656+carlacastilhos@users.noreply.github.com>
2020-12-17 18:27:19 -0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -101,7 +101,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'FAQLOGINUNICO'
+htmlhelp_basename = 'FAQCFEM'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -128,7 +128,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'ManualScriptChatbotProtocoloDigital.tex', u'FAQ Protocolo Digital',
+    (master_doc, 'ManualScriptChatbotProtocoloDigital.tex', u'FAQ CFEM',
      u'ANM', 'manual'),
 ]
 
@@ -138,7 +138,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'manualscriptchatbotProtocoloDigital', u'FAQ Protocolo Digital',
+    (master_doc, 'manualscriptchatbotProtocoloDigital', u'FAQ CFEM',
      [author], 1)
 ]
 
@@ -149,7 +149,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'ManualScriptChatbotProtocoloDigital', u'FAQ Protocolo Digital',
+    (master_doc, 'ManualScriptChatbotProtocoloDigital', u'FAQ CFEM',
      author, 'ManualScriptChatbotProtocoloDigital', 'One line description of project.',
      'Miscellaneous'),
 ]
